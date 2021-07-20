0d6daabe8981962e7775951539708fb1bd90ba75
Guillaume EHRET <guillaume@dev-mind.fr>
2021-07-20 11:06:00 +0200

    Initialize project

---
+++ app/src/main/java/org/mixitconf/MainActivity.kt
@@ -0,0 +1,15 @@
+package org.mixitconf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun openLoginPage() {
+
+    }
+}

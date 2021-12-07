1416950fc1c1629ba49a0279484d4a893f93029f
Guillaume EHRET <guillaume@dev-mind.fr>
2021-12-07 23:13:52 +0100

    Remove login service and screen

--- app/src/main/java/org/mixitconf/service/ServiceModule.kt
+++ app/src/main/java/org/mixitconf/service/ServiceModule.kt
@@ -1,11 +1,9 @@
 package org.mixitconf.service
 
-import android.accounts.AccountManager
 import androidx.preference.PreferenceManager
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import org.mixitconf.service.planning.PlanningAlarmService
-import org.mixitconf.service.security.LoginService
 import org.mixitconf.service.synchronization.SpeakerSynchronizationService
 import org.mixitconf.service.synchronization.TalkSynchronizationService
 
@@ -13,15 +11,6 @@ import org.mixitconf.service.synchronization.TalkSynchronizationService
  * Dependency module for the data module.
  */
 val serviceModule = module {
-    /**
-     * Declare a single instance of [AccountManager].
-     */
-    single { AccountManager.get(androidContext()) }
-
-    /**
-     * Declare a single instance of [LoginService].
-     */
-    single { LoginService(get(), get(), get()) }
 
     /**
      * Declare a single instance of [SpeakerSynchronizationService].
--- app/src/main/java/org/mixitconf/ui/UiModule.kt
+++ app/src/main/java/org/mixitconf/ui/UiModule.kt
@@ -2,7 +2,6 @@ package org.mixitconf.ui
 
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import org.mixitconf.ui.login.LoginViewModel
 import org.mixitconf.ui.settings.FavoritesViewModel
 import org.mixitconf.ui.speaker.SpeakersViewModel
 import org.mixitconf.ui.talk.TalksViewModel
@@ -11,13 +10,6 @@ import org.mixitconf.ui.talk.TalksViewModel
  * Dependency module for the ui module.
  */
 val uiModule = module {
-
-    /**
-     * Declare a single instance of [LoginViewModel].
-     */
-    viewModel {
-        LoginViewModel(get())
-    }
 
     /**
      * Declare a single instance of [TalksViewModel].
